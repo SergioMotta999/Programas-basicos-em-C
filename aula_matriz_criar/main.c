73ee5b8fa0a4a0d9fac18e8b95cab52bbd735aa1
sergio mota <serginhomota77@gmail.com>
2022-06-20 10:35:57 -0300

    Add files via upload

---
+++ aula_matriz_criar/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Como criar uma matriz
+
+int main()
+{
+ int vet[];
+ int mat[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+ /*      c0 c1 c3
+ linha 0  1  2  3
+ linha 1  4  5  6
+ linha 2  7  8  9
+ */
+
+ int mat1[3][] = {1, 2, 3, 4, 5, 6, 7}; // Preenche o resto com 0
+ int mat2[3][3] = {0};
+
+ float mat3[4][5];
+ char mat5[10][10];
+
+}
